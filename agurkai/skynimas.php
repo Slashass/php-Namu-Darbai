976c0e1c551d7ca8184aade81c310ec63dbd5de0
Slashass <70883938+Slashass@users.noreply.github.com>
2021-01-06 22:33:47 +0200

    not counting need fix

--- agurkai/skynimas.php
+++ agurkai/skynimas.php
@@ -1,46 +1,25 @@
 <?php
-// saugo sesijoje
 session_start();
-
-// pradinis masyvas ir ID nustatymas
-if (!isset($_SESSION['a'])) {
-    $_SESSION['obj'] = [];
-    $_SESSION['agurku ID'] =  0;
-}
-
+include __DIR__ . '/App.php';
+include __DIR__ . '/Darzove.php';
 include __DIR__ . '/Agurkas.php';
+include __DIR__ . '/Paprika.php';
 
+App::setSession();
 
-// skinti kazkoki kieki irasius
 if (isset($_POST['skinti'])) {
-    foreach ($_SESSION['obj'] as $index => &$agurkas) {
-        if ($_POST['kiek'][$agurkas->id] <= $agurkas->count) {
-            $agurkas->count -= $_POST['kiek'][$agurkas->id];
-        }
-    }
-    header('Location: http://localhost/1stlesson/folder/agurkai/skynimas.php');
-    exit;
-}
-// skinti visus 
-if (isset($_POST['skinti-visus'])) {
-    foreach ($_SESSION['obj'] as &$agurkas) {
-        if ($_POST['skinti-visus'] == $agurkas->id) {
-            $agurkas->count = 0;
-        }
-    }
-    header('Location: http://localhost/1stlesson/folder/agurkai/skynimas.php');
-    exit;
+    App::harvest();
+    App::redirect('skynimas');
 }
 
-// nuimti visa derliu
+if (isset($_POST['skinti-visus'])) {
+    App::harvestOne();
+    App::redirect('skynimas');
+}
+
 if (isset($_POST['nuskinti-viska'])) {
-    foreach ($_SESSION['obj'] as &$agurkas) {
-        if ($_POST['kiek'][$agurkas->id] <= $agurkas->count) {
-            $agurkas->count = 0;
-        }
-    }
-    header('Location: http://localhost/1stlesson/folder/agurkai/skynimas.php');
-    exit;
+    App::harvestAll();
+    App::redirect('skynimas');
 }
 
 ?>
@@ -53,7 +32,8 @@ if (isset($_POST['nuskinti-viska'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Skynimas</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/reset.css">
 </head>
 
 <body>
@@ -63,21 +43,41 @@ if (isset($_POST['nuskinti-viska'])) {
     <a href="auginimas.php">Auginimas</a>
     <a href="skynimas.php">Skynimas</a>
     <form action="" method="post">
-        <?php foreach ($_SESSION['obj'] as $agurkas) : ?>
-            <?php $agurkas = unserialize($agurkas) ?>
-            <div>
-                <img src="./img/cuc1.jpg" alt="Agurko nuotrauka">
-                Agurkas nr. <?= $agurkas->id ?>
-                Galima skinti: <?= $agurkas->count ?>
-                <input type="text" name="kiek[<?= $agurkas->id ?>]" value=<?= $skynimas ?? 0 ?>>
-                <button type="submit" name="skinti">SKINTI</button>
-                <button type="submit" name="skinti-visus" value="<?= $agurkas->id ?>">SKINTI VISUS</button>
-
-            </div>
-
+        <?php foreach ($_SESSION['darzoves'] as $darzove) : ?>
+            <?php $darzove = unserialize($darzove) ?>
+            <?php if ($darzove instanceof Agurkas) : ?>
+                <div class="items skynimas">
+                    <img src="./img/cuc-<?= $darzove->imgPath ?>.jpg" alt="Agurko nuotrauka">
+                    <?php if ($darzove->count == 0) : ?>
+                        <h2>Agurkas nr. <?= $darzove->id ?></h2>
+                        <h3>Kiekis: <span><?= $darzove->count ?></span></h3>
+                        <p>Nėra ko skinti.</p>
+                    <?php else : ?>
+                        <p>Galima skinti: <?= $darzove->count ?></p>
+                        <input class="kiek" type="text" name="kiekis">
+                        <button class="skinti" type="submit" name="skinti" value="<?= $darzove->id ?>">Skinti</button>
+                        <button class="skinti-visus" type="submit" name="skinti-visus" value="<?= $agurkas->id ?>">Skinti visus</button>
+                    <?php endif ?>
+                </div>
+            <?php else : ?>
+                <div class="items skynimas">
+                    <img src="./img/paprika-<?= $darzove->imgPath ?>.jpg" alt="Paprikos nuotrauka">
+                    <?php if ($darzove->count == 0) : ?>
+                        <h2>Paprika nr. <?= $darzove->id ?></h2>
+                        <h3>Kiekis: <span><?= $darzove->count ?></span></h3>
+                        <p>Nėra ko skinti.</p>
+                    <?php else : ?>
+                        <p>Galima skinti: <?= $darzove->count ?></p>
+                        <input class="kiek" type="text" name="kiek">
+                        <button class="skinti" type="submit" name="skinti" value="<?= $darzove->id ?>">Skinti</button>
+                        <button class="skinti-visus" type="submit" name="skinti-visus" value="<?= $darzove->id ?>">Skinti visus</button>
+                    <?php endif ?>
+                </div>
+            <?php endif ?>
         <?php endforeach ?>
-        <button type="submit" name="nuskinti-viska">Nuskinti visus agurkus</button>
-    </form>
+        <form class="nuskinti-viska" action="" method="post">
+            <button class="nuskinti-viska" type="submit" name="nuskinti-viska">Nuskinti visus agurkus</button>
+        </form>
 </body>
 
 </html>
