cedaad94e4de75d28da515e1e55c38b8ff40bf96
Slashass <70883938+Slashass@users.noreply.github.com>
2021-01-24 13:41:06 +0200

    index

--- simple-mvc/index.php
+++ simple-mvc/index.php
@@ -4,7 +4,7 @@ require_once('Routes.php');
 
 function my_autoloader($class_name)
 {
-    include 'classes/' . $class_name . '.class.php';
+    include './classes/' . $class_name . '.php';
 }
 
 spl_autoload_register('my_autoloader');
